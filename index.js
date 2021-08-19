dd0852695db92c1d9d0b73ff4bbfbe67e2a7902d
Andrew <slonryba@gmail.com>
2021-08-19 21:23:34 +0300

    rename some vars

--- index.js
+++ index.js
@@ -14,68 +14,68 @@ export default () => {
 
   app.use(bodyParser.urlencoded({ extended: false }));
 
-  app.get('/', (req, res) =>{
-    res.render('index');
+  app.get('/', (request, responce) =>{
+    responce.render('index');
   })
 
-  app.get('/routes/:id', (req, res) => {
-    const cont = req.params.id;
-    res.send(`id.parameter -- ${cont}`);
+  app.get('/routes/:id', (request, responce) => {
+    const cont = request.params.id;
+    responce.send(`id.parameter -- ${cont}`);
   })
 
-  app.get('/some', (req, res) => {
-    res.send('<h3>jojo</h3>');
+  app.get('/some', (request, responce) => {
+    responce.send('<h3>jojo</h3>');
   })
 
-  app.get('/retrieve', (req, res) => {
+  app.get('/retrieve', (request, responce) => {
     const inp = [];
-    res.render('posts/scr', { inp });
+    responce.render('posts/scr', { inp });
   })
 
-  app.post('/retrieve', (req, res) => {
-    const { id } = req.body;
-    res.send(`--|| ${id} ||--`);
+  app.post('/retrieve', (request, responce) => {
+    const { id } = request.body;
+    responce.send(`--|| ${id} ||--`);
   })
 
-  app.get('/posts', (req, res) => {
+  app.get('/posts', (request, responce) => {
     const form = [];
-    res.render('posts/form', { form });
+    responce.render('posts/form', { form });
   })
     
-  app.post('/posts', (req, res) => {
-    const { name, gender, email, country } = req.body;
+  app.post('/posts', (request, responce) => {
+    const { name, gender, email, country } = request.body;
     const query = 'INSERT INTO citizens (name, gender, email, country) VALUES ($1, $2, $3, $4)';
     const values = [name, gender, email, country];
 
     pool
       .query(query, values)
-      .then(results => res.send(`note was added <a href='/'>back to main page`))
+      .then(results => responce.send(`note was added <a href='/'>back to main page`))
       .catch(error => console.error('note does not was added'))
   })
 
-  app.get('/screen', (req, res) => {
+  app.get('/screen', (request, responce) => {
     pool
       .query('SELECT * FROM citizens')
       .then(results => {
         const list = results.rows;
-        res.render('posts/screen', { list });    
+        responce.render('posts/screen', { list });    
       })
       .catch(error => console.error('the page cannot be displayed'))
   })
 
-  app.get('/del', (req, res) => {
+  app.get('/del', (request, responce) => {
     const note = [];
-    res.render('posts/delete', { note });        
+    responce.render('posts/delete', { note });        
   })
 
-  app.post('/del', (req, res) => {
-    const { name } = req.body;
+  app.post('/del', (request, responce) => {
+    const { name } = request.body;
     const data = [name];
     const query = 'DELETE FROM citizens WHERE name = $1';        
 
     pool
       .query(query, data)
-      .then(results => res.send(`note by name ${name} was deleted <a href='/'> back to main page`))
+      .then(results => responce.send(`note by name ${name} was deleted <a href='/'> back to main page`))
       .catch(error => console.error('the note cannot be removed'))    
   })
 
